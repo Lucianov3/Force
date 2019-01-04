2eb759b3852fae3e6e00b2225eacf6ebf6612c1d
Sankowski <thomas-sankowski@t-online.de>
2019-01-04 13:04:50 +0100

    add: fancyshit

---
+++ Magnets Test/Assets/PseudoDoorScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PseudoDoorScript : MonoBehaviour
+{
+    private bool activated = false;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        activated = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+    }
+}
---
+++ Magnets Test/Assets/Scripts/MapEditor/MapEditorScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapEditorScript : MonoBehaviour
+{
+    public Vector2 gizmoPosition;
+    public float depth = 0;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(new Vector3(gizmoPosition.x, gizmoPosition.y, depth), new Vector3(1, 1, 1));
+    }
+}
