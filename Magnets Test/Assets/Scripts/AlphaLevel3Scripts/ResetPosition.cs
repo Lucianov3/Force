5425708b6cdf78e79c9267f5be55d0309ad9b647
Sankowski <thomas-sankowski@t-online.de>
2019-01-07 12:13:56 +0100

    add: alphalevel scripts

---
+++ Magnets Test/Assets/Scripts/AlphaLevel3Scripts/ResetPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 playerPostion;
+
+    private void Start()
+    {
+        playerPostion = Player.transform.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Player.transform.position = playerPostion;
+        }
+    }
+}
