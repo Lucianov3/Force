7dfefaddad83b38235483a05dda369812fdf0272
Dennis <DennisFranke96@gmx.de>
2019-01-08 16:53:37 +0100

    Add: Level Class, Loading and Saving Levels, Level Editor

---
+++ Magnets Test/Assets/Scripts/LevelEditor/Level.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class Level
+{
+    public enum LevelOrientation { UP, DOWN }
+
+    public string Name = "";
+
+    public LevelContent[,,] Content = new LevelContent[2, 63, 19];
+
+    public LevelOrientation TopOrientation = LevelOrientation.DOWN;
+    public LevelOrientation BotOrientation = LevelOrientation.UP;
+
+    public Level()
+    {
+        FillLevelWithContent();
+    }
+
+    public void FillLevelWithContent()
+    {
+        for (int i = 0; i < Content.GetLength(0); i++)
+        {
+            for (int j = 0; j < Content.GetLength(1); j++)
+            {
+                for (int k = 0; k < Content.GetLength(2); k++)
+                {
+                    Content[i, j, k] = new LevelContent();
+                }
+            }
+        }
+    }
+
+    public void LoadLevelFromJson(string path)
+    {
+        using (StreamReader reader = File.OpenText(path))
+        {
+            JObject o = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+            Level level = JsonConvert.DeserializeObject<Level>(o.ToString());
+            this.Name = level.Name;
+            this.Content = level.Content;
+            this.TopOrientation = level.TopOrientation;
+            this.BotOrientation = level.BotOrientation;
+        }
+    }
+
+    public void SaveLevelToJson(string path)
+    {
+        File.WriteAllText(path, JsonConvert.SerializeObject(this));
+
+        // write JSON directly to a file
+        using (StreamWriter file = File.CreateText(path))
+        using (JsonTextWriter writer = new JsonTextWriter(file))
+        {
+            JObject.FromObject(this).WriteTo(writer);
+        }
+    }
+}
---
+++ Magnets Test/Assets/Scripts/LevelEditor/LevelEditorScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEditorScript : MonoBehaviour
+{
+    [SerializeField] private GameObject pointer;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
