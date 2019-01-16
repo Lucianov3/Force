21a9fa870768c13380daca52288069670b70eba2
Sankowski <thomas-sankowski@t-online.de>
2019-01-16 14:42:48 +0100

    add:main menu; add scenes

---
+++ Magnets Test/Assets/MainMenuButtonScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuButtonScript : MonoBehaviour
+{
+    public void Scene(string buttonPress)
+    {
+        SceneManager.LoadScene(buttonPress);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
