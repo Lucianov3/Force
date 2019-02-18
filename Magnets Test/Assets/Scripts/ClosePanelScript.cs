fb9a54a037743a4d9972295e17651aa97c7fc866
Sankowski <thomas-sankowski@t-online.de>
2019-02-18 12:46:13 +0100

    add: new scenes; add: new materials; add: new textures; add: new Prefabs; add: new player prefabs

---
+++ Magnets Test/Assets/Scripts/ClosePanelScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosePanelScript : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;
+
+    private void Update()
+    {
+        ClosePanel();
+    }
+
+    public void ClosePanel()
+    {
+        if (Input.GetButtonDown("Menu A Button"))
+        {
+            panel.SetActive(false);
+        }
+    }
+}
